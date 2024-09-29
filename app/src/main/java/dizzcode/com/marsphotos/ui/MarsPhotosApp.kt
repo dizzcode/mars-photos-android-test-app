756dacf76f27cef9f8c94d5b77b31b558c085dc9
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-29 14:14:54 +0530

    Add resources

---
+++ app/src/main/java/dizzcode/com/marsphotos/ui/MarsPhotosApp.kt
@@ -0,0 +1,55 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package dizzcode.com.marsphotos.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dizzcode.com.marsphotos.R
+import dizzcode.com.marsphotos.ui.screens.HomeScreen
+import dizzcode.com.marsphotos.ui.screens.MarsViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MarsPhotosApp() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { MarsTopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val marsViewModel: MarsViewModel = viewModel()
+            HomeScreen(
+                marsUiState = marsViewModel.marsUiState,
+                contentPadding = it,
+            )
+        }
+    }
+}
+
+@Composable
+fun MarsTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        },
+        modifier = modifier
+    )
+}
